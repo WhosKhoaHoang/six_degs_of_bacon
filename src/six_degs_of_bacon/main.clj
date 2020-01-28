8c4976e80c94e861c0b5375193ca417441197dba
Khoa Hoang <whoskhoahoang@gmail.com>
2020-01-28 23:00:31 +0900

    Implement user input prompt.

--- src/six_degs_of_bacon/main.clj
+++ src/six_degs_of_bacon/main.clj
@@ -7,12 +7,28 @@
   "I don't do a whole lot ... yet."
   [& args]
 
-  (comment "Verify if there's a request limit:" comment)
+  ;"Verify if there's a request limit
   (comment dotimes [i 10]
       (comment TMDB/personSearch "Arnold" comment)
    comment)
 
+  (do
+      (print "Actor: ")
+      (flush)
+      (def actor (read-line))
+  )
+  (println actor)
+ 
+  ;TODO: Provide some validation?
+  ;TODO: Allow users to find path between any two actors
+  ;      (not just to Kevin Bacon)
+  ;CONSIDER: Somehow make the solution to the 6 Degrees
+  ;          of Kevin Bacon problem reusable for other
+  ;          similar problems.
+  ;TARGET OUTPUT:
+  ;Actor's Bacon number and their path towards Bacon
+
   (try
-     (println (TMDB/personSearch "Arnold"))
+     ;(println (TMDB/personSearch "Arnold"))
      (catch Exception e (println e)))
 )
