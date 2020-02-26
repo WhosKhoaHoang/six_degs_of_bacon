63e2dcc8a2b012a904096f5216e7d91b53925410
Khoa Hoang <whoskhoahoang@gmail.com>
2020-02-26 21:43:31 +0900

    Implement a baseline BFS function.
    Any other implementations of BFS for this program
    can draw inspiration from this baseline function.

--- src/six_degs_of_bacon/main.clj
+++ src/six_degs_of_bacon/main.clj
@@ -17,8 +17,9 @@
 )
 
 
-;THINK: Can use the result of this function to build the "accesses" map?
-(defn concat-kids
+;NOTE: concat is being used to put characters at the
+;      end of my-q. Eventually consider a more general approach?
+(defn concat-children
     [my-q g visited]
     (concat (rest my-q) (for [child (get g (str (first my-q)))
                          :when (not (contains? visited child))] child))
@@ -27,120 +28,42 @@
     [my-q g accesses]
     (into accesses (for [e (get g (str (first my-q)))] [e (str (first my-q))]))
 )
-;TODO: Add "accesses" variable
-; ===== FOR REFERENCE ===== ;
-(comment
-;When target has been found:
-(let [p-node (atom @cur-node)]        
-    (reset! path (conj @path @p-node))
-    (while (not (= @p-node start))
-        (do
-            (reset! path (conj @path (@accesses @p-node)))
-            (reset! p-node (@accesses @p-node))
-        )
-    )
-    ;Empty out my-q in order to prevent
-    ;outermost while loop from executing
-    ;again (a break would be good, but
-    ;Clojure doesn't give you a break lol)
-    (reset! my-q (empty @my-q))
-    (reset! path (reverse @path))
-)
-;When target has not been found:
-(doseq [child (g @cur-node)]
-;for is "lazy" and won't run so use doseq instead.
-    (do
-        (reset! accesses (assoc @accesses child @cur-node))
-        (reset! my-q (conj @my-q child))
+(defn make-path
+    [accesses start path]
+    (if (= (last path) start)
+        (let [res (reverse path)] res)
+        (recur accesses start (conj path (get accesses (str (last path)))))
     )
 )
-comment)
-(defn bfs-c2
+(defn base-bfs
     [g start targ]
     """
-    A more Clojure-ish approach to BFS
+    Searches for a target node in a graph (represented by an
+    adjacency map) using a breadth-first search. Use this im-
+    plementation as a baseline reference for any other BFS
+    implementations.
+    @g: The graph (represented by an adjacency map) to search
+    @start: The start node to begin searching from
+    @target: The target node to search for
+    type g: map
+    type start: str
+    type target: str
+    return: The path taken to reach the target node
+    rtype: vector
     """
     (loop [my-q (concat () start)
            visited #{}
            accesses {}
            cnt 0]
         (if (= (str (first my-q)) targ)
-            ;When target is acquired, add target to
-            ;visited as well and build accesses map
-
-            ;THINK: check the last element in path and see if it equals start
-            (println "\nTARGET ACQUIRED\n" accesses visited targ cnt)
-
-            (recur (concat-kids my-q g visited)
+            ;Note how targ is NOT added to visited.
+            ;This may not actually be a problem.
+            (make-path accesses start [targ])
+            (recur (concat-children my-q g visited)
                    (conj visited (first my-q))
                    (update-accesses my-q g accesses)
                    (inc cnt))
-            )
         )
-)
-
-
-(defn bfs
-    "
-    Searches for a target in a graph (represented
-    by an adjacency map) using a breadth-first-search.
-    @g: The graph (represented by an adjacency map) to search
-    @start: The start node to begin searching from
-    @target: The target node to search for
-    "
-    [g start target]
-
-    ;TODO: Use a more Clojurey approach to this implementation
-    ;      (feels more imperative than functional)
-
-    (let [my-q (atom '()) ;Use list because pop() occurs in front
-          visited (atom #{})
-          accesses (atom {})
-          path (atom [])]
-         (reset! my-q (conj @my-q start))
-         ;conj to my-q because initializing my-q
-         ;with start only puts the SYMBOL start, NOT
-         ;its value!
-         (while (not-empty @my-q)
-            (let [cur-node (atom (nth @my-q 0))]
-                (reset! my-q (pop @my-q))
-                (reset! visited (conj @visited @cur-node))
-                (if (= (str @cur-node) target)
-                    (do
-                        (let [p-node (atom @cur-node)]        
-                            (reset! path (conj @path @p-node))
-                            (while (not (= @p-node start))
-                                (do
-                                    (reset! path (conj @path (@accesses @p-node)))
-                                    (reset! p-node (@accesses @p-node))
-                                )
-                            )
-                            ;Empty out my-q in order to prevent
-                            ;outermost while loop from executing
-                            ;again (a break would be good, but
-                            ;Clojure doesn't give you a break lol)
-                            (reset! my-q (empty @my-q))
-                            (reset! path (reverse @path))
-                        )
-                    )
-                    ;Else
-                    (do
-                        (doseq [child (g @cur-node)]
-                        ;for is "lazy" and won't run so use doseq instead.
-                            (do
-                                (reset! accesses (assoc @accesses child @cur-node))
-                                (reset! my-q (conj @my-q child))
-                            )
-                        )
-                    )
-                )
-            )
-         )
-        (let [res @path] res)
-        ;In Clojure, the return value of a function
-        ;is the last form evaluated inside the function.
-        ;Make this line the function's last form evaluated
-        ;by writing it here.
     )
 )
 
@@ -149,14 +72,9 @@ comment)
     "I don't do a whole lot ... yet."
     [& args]
 
-    ;"Verify if there's a request limit...Apparently no limit.
-    (comment dotimes [i 10]
-        (comment TMDB/personSearch "Arnold" comment)
-    comment)
-
     (do
         (print "Actor: ")
-        (flush)
+        (flush) ;"Flush" the "Actor" string to stdout
         (def actor (read-line))
     )
     (println (str "The name of the actor is " actor))
@@ -177,14 +95,10 @@ comment)
     )
     comment)
 
-    ;(get-kids { "a" ["b" "c"] "b" [] "c" ["d"] "d" []} "a")
-    ;(def res (bfs { "a" ["b" "c"] "b" [] "c" ["d"] "d" []} "a" "d"))
-
-    (def res (bfs-c2 { "a" ["b" "c"] "b" [] "c" ["d"] "d" []} "a" "d"))
-    ;(def res (bfs-c2 { "a" ["b" "c"] "b" ["d"] "c" ["b"] "d" ["e"] "e" []} "a" "e"))
+    (def res (base-bfs { "a" ["b" "c"] "b" [] "c" ["d"] "d" []} "a" "d"))
+    ;(def res (base-bfs { "a" ["b" "c"] "b" ["d"] "c" ["b"] "d" ["e"] "e" []} "a" "e"))
     ;b has two parent nodes here^
-
-    ;(println res)
+    (println res)
 
     ;TODO: Provide some validation?
     ;TODO: Allow users to find path between any two actors
